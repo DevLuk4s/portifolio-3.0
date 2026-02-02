7c4c430e891cf0d3ba29f1890b0e0bbec421070f
Lucas Gabriel <lucasgabrielferreirasilva777@gmail.com>
2026-02-02 03:13:58 -0300

    Footer
    criador Footer do meu portfolio

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Projects />
+      <Footer />
     </div>
   );
 }
