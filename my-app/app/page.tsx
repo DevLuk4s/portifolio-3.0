629c5f678fab9c111c249cf2f9913ea9ad3a440d
Lucas Gabriel <lucasgabrielferreirasilva777@gmail.com>
2026-02-04 09:12:20 -0300

    Newsletter Section
    criador area Newsletter

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import Newsletter from "./components/Newsletter";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Projects />
       <Footer />
+      <Newsletter />
     </div>
   );
 }
